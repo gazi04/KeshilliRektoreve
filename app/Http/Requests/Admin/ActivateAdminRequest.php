3b7a5ddeab58162acf7d9967de3ee518e67b57ac
gazi04 <gazmendhalili2016@gmail.com>
2025-05-30 00:02:02 +0200

    :safety_vest: Added validation form requests for 'User Management'
    This commit adds the validation layer that will be used in the admin
    controller to filter out the request from the front end part. This
    commit includes validation for:
     1. Creating Admins
     2. Editing Admins
     3. Activate Admins
     4. Deactivate Admins

---
+++ app/Http/Requests/Admin/ActivateAdminRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ActivateAdminRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'id' => 'required|exists:admins,id'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'id.required' => 'ID është e detyrueshme.',
+            'id.exists' => 'ID e dhënë nuk egziston në tabelën e administratorëve.',
+        ];
+    }
+}
