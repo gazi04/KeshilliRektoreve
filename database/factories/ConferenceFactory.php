859c2e39459f839dcabe7b256e38cf8883748c4e
gazi04 <gazmendhalili2016@gmail.com>
2025-05-31 20:29:20 +0200

    :card_file_box: Added model factories
    This commits adds factories for all the models used through this project
    for testing purposes, and also updated the seeder to call those
    factories.

--- database/factories/ConferenceFactory.php
+++ database/factories/ConferenceFactory.php
@@ -17,7 +17,36 @@ class ConferenceFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => $this->faker->sentence(3),
+            'date' => $this->faker->dateTimeBetween('now', '+1 year'),
         ];
+    }
+
+        public function withDocuments(int $count = 1): static
+    {
+        return $this->afterCreating(function (\App\Models\Conference $conference) use ($count) {
+            \App\Models\Document::factory()
+                ->count($count)
+                ->forConference($conference)
+                ->create();
+        });
+    }
+
+    public function past(): static
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'date' => $this->faker->dateTimeBetween('-1 year', '-1 day'),
+            ];
+        });
+    }
+
+    public function future(): static
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'date' => $this->faker->dateTimeBetween('+1 day', '+1 year'),
+            ];
+        });
     }
 }
--- database/factories/DocumentFactory.php
+++ database/factories/DocumentFactory.php
@@ -16,8 +16,52 @@ class DocumentFactory extends Factory
      */
     public function definition(): array
     {
+        $types = ['agenda', 'minutes', 'presentation', 'report', 'other'];
+
         return [
-            //
+            'title' => $this->faker->sentence(2),
+            'url' => $this->faker->url(),
+            'type' => $this->faker->randomElement($types),
+            'conferenceId' => \App\Models\Conference::factory(),
         ];
+    }
+
+    public function forConference(\App\Models\Conference $conference): static
+    {
+        return $this->state(function (array $attributes) use ($conference) {
+            return [
+                'conferenceId' => $conference->id,
+            ];
+        });
+    }
+
+    public function agenda(): static
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'type' => 'agenda',
+                'title' => 'Agenda: ' . $this->faker->words(3, true),
+            ];
+        });
+    }
+
+    public function minutes(): static
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'type' => 'minutes',
+                'title' => 'Minutes: ' . $this->faker->words(3, true),
+            ];
+        });
+    }
+
+    public function presentation(): static
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'type' => 'presentation',
+                'title' => $this->faker->catchPhrase(),
+            ];
+        });
     }
 }
